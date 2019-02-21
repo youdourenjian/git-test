de4d2c2890d5545b3268325a48fb09240a756c63
zhangsan <zhangsan@itcast.cn>
2019-02-21 11:31:28 +0800

    添加test文件 把本地工程添加到git中

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Gtit Project");
+        System.out.println("Hello Gtit Project");
+        System.out.println("Hello Gtit Project");
+
+    }
+    public void test(){
+        int i=1;
+        i=i+10;
+        System.out.println(i);
+    }
+}
